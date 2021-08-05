76d80e0ed8e57327ad93bb9d773d0a4260ff3820
Chris McGee <newton688@gmail.com>
2021-08-04 22:49:26 -0400

    Add SSH address capability to gemini command
    Move status code lines to stderr instead of stdout

--- cmd/gemini/README.md
+++ cmd/gemini/README.md
@@ -1,14 +1,16 @@
 # Gemini Command
 
 The gemini command invokes a Gemini transaction with either on either a Gemini
-URL, Gemini SSH  or a local file path. In the case of the SSH URL it will initiate
-the connection over SSH and invoke the transaction on the remote host using
+URL, Gemini SSH  or a local file path. In the case of the gemcap URL it will
+initiate the connection over SSH and invoke the transaction on the remote host using
 this command, but with a file path. Also, in the latter case the gemini command
 will set up any extra SSH configuration for the remote host to set up capsule
-SSH access, such as a public key if none exists.
+SSH access, such as a public key if none exists. Note that SSH-style addresses
+can also be used instead of URL's.
 
 ```
 gemini [gemcap|gemini]://[username@]somehost/some/path
+gemini [username@]somehost:/some/path
 ```
 
 Note that if no username is provided then it will be assumed that it is
@@ -26,10 +28,17 @@ gemini /some/path
 ```
 
 When the command is invoked on a local file path it will return a well-formed
-Gemini protocol response. If the local file path exists and can be read by the
-current user then a 20 status is returned with the media type along with the
-contents of the file. Otherwise, a 51 (Not Found) status is returned. Success
-statuses will exit with code 0, any non-success statuses exit with the code
+Gemini protocol response, except that the status line is sent to stderr and
+the content is sent to stdout. This is done to allow independent redirection
+of the content from status and warning messages. Also, it follows established
+C and UNIX standards so that this command works more like many command-line
+tools. The well-defined status line allows gemini-aware clients to parse it
+and react in different ways depending on the status code.
+
+If the local file path exists and can be read by the current user then a 20
+status is returned with the media type along with the contents of the file.
+Otherwise, a 51 (Not Found) status is returned. Success statuses will exit
+with code 0, any non-success statuses exit with the code
 set to the status integer.
 
 ```
@@ -41,8 +50,11 @@ If the path is a directory then a check will be performed whether there is an
 index.gmi file in that location that is readable by the current user in which
 case the status is 20 and the file contents will be sent.
 
+## Server mode
+
 When the gemini command is invoked with the server flag it starts a gemini
-server for a local content path.
+server and supports simple read access to the filesystem with the capsule
+content path as the root.
 
 ```
 gemini --server [--listen-address <address>] <cert> <key> <capsule-content-path>
--- cmd/gemini/main.go
+++ cmd/gemini/main.go
@@ -11,10 +11,11 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
 var CLI struct {
-	Path          string `arg name:"path" help:"The path/URL of the gemini resource to start a transaction or the root of a capsule to start a server." required""`
+	Path          string `arg name:"path" help:"The path/URL/SSH address of the gemini resource to start a transaction or the root of a capsule to start a server." required""`
 	ListenAddress string `flag name:"listen-address" help:"Start a gemini server and listen on this address and the provided path as the root of the capsule. Example: --listen-address=:1965"`
 	HostCertPEM   string `flag name:"host-cert" help:"The path to the host cert in PEM format."`
 	HostKeyPEM    string `flag name:"host-key" help:"The path to the host private key in PEM format."`
@@ -77,6 +78,7 @@ func main() {
 	kong.Parse(&CLI)
 
 	p := CLI.Path
+	ps := strings.Split(p, "/")
 
 	if CLI.ListenAddress != "" {
 		if CLI.HostCertPEM == "" || CLI.HostKeyPEM == "" {
@@ -163,14 +165,76 @@ func main() {
 				panic(err)
 			}
 		}
+	} else if len(ps) > 1 && strings.Contains(ps[0], ":") {
+		// SSH style addresses
+		userhost := ps[0]
+		userhost = userhost[:len(userhost)-1]
+		host := userhost
+		username := "capsule"
+		if strings.Contains(userhost, "@") {
+			uhs := strings.Split(userhost, "@")
+			if len(uhs) != 2 {
+				fmt.Fprintf(os.Stderr, "Invalid path")
+				os.Exit(127)
+			}
+			username = uhs[0]
+			host = uhs[1]
+		}
+		path := p[len(userhost)+1:]
+		if path == "" {
+			path = "/"
+		}
+
+                // We do some special setup for capsule access, otherwise,
+                //  we just use the usual configuration
+                if username == "capsule" {
+                        err := setup.AssertCapsuleConfig(host)
+                        if err != nil {
+                                panic(err)
+                        }
+                }
+
+                // TODO more sanitization of the path in addition to the server sanitization
+
+                cmd := exec.Command("ssh", fmt.Sprintf("%s@%s", username, host), "gemini", path)
+
+                stdout, err := cmd.StdoutPipe()
+                if err != nil {
+                        panic(err)
+                }
+                stderr, err := cmd.StderrPipe()
+                if err != nil {
+                        panic(err)
+                }
+
+                if err := cmd.Start(); err != nil {
+                        panic(err)
+                }
+
+                go func() {
+                        defer stdout.Close()
+                        io.Copy(os.Stdout, stdout)
+                }()
+                go func() {
+                        defer stderr.Close()
+                        io.Copy(os.Stderr, stderr)
+                }()
+
+                if err := cmd.Wait(); err != nil {
+                        if _, ok := err.(*exec.ExitError); !ok {
+                                panic(err)
+                        }
+                }
+
+                os.Exit(cmd.ProcessState.ExitCode())
 	} else if err == nil && u.Scheme != "" {
-		fmt.Printf("Only gemcap:// URL scheme is supported\n")
+		fmt.Printf("Only gemcap:// and gemini:// URL schemes are supported\n")
 		os.Exit(127)
 	} else {
 		req := gemini.Request{}
 		h := handler{p}
 		resp := h.Handle(req)
-		fmt.Printf("%d %s\r\n", resp.Status, resp.Meta)
+		fmt.Fprintf(os.Stderr, "%d %s\r\n", resp.Status, resp.Meta)
 		if resp.Status > 29 {
 			os.Exit(resp.Status)
 		}
